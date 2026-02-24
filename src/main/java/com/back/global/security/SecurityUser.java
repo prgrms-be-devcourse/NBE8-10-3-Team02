3afa11211189960118fe1769070bffc65b6bc392
장인호 <jih95349238@gmail.com>
2026-02-24 17:35:58 +0900

    Refactor:#5 post comments 마이그레이션

---
+++ src/main/java/com/back/global/security/SecurityUser.java
@@ -0,0 +1,34 @@
+package com.back.global.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class SecurityUser extends User {
+    private final int id;
+
+    @Getter
+    private final String email;
+
+    @Getter
+    private final String nickname;
+
+    public SecurityUser(
+            int id,
+            String email,
+            String nickname,
+            String password,
+            Collection<? extends GrantedAuthority> authorities
+    ) {
+        super(email, password, authorities);
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+}
